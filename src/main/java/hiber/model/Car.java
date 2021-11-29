3b5de94152b706df77af82497accf80c94088f8e
Artem Shinkarenko <tomikus1@gmail.com>
2021-11-29 20:09:38 +0300

    car added

---
+++ src/main/java/hiber/model/Car.java
@@ -0,0 +1,19 @@
+package hiber.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cars")
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "model")
+    String model;
+
+    @Column(name = "series")
+    int series;
+
+
+}
